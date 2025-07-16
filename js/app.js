bdaed2e12160c2d685d34da65d21f65749358374
hinata828-art <hinatasan828@gmail.com>
2025-07-16 10:24:17 +0900

    Update app.js

--- js/app.js
+++ js/app.js
@@ -4,19 +4,18 @@ $(function() {
     $('#btn').on('click', function(){
         $.ajax({
             //リクエスト先のURLを設定
-
+                url: "http://zipcloud.ibsnet.co.jp/api/serch?zipcode=" + $('#zipcode').val(),
             //レスポンスのデータ形式を設定
-
+                dataType:'jsonp',
         }).done(function(data){
             //通信が成功したときの処理
             if(data.results){
                 //データが取得できたとき、setData関数を呼び出す
-
+                setData(data.results[0]);
             }else{
                 alert("該当するデータが見つかりませんでした");
             }
         }).fail(function(data){
-            //通信が失敗したときの処理
             alert("通信に失敗しました");
         });
     });
@@ -24,5 +23,7 @@ $(function() {
 
 function setData(data){
     //取得データを各HTML要素に代入
-
+        $('#prefecture').val(data.address1);
+        $('#city').val(data.address2);
+        $('#address').val(data.address3);
 }
